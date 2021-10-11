52dd85d736261b3ddc821e8ee5e9f481c4eded4a
Gabriel <storkdeveloper@gmail.com>
2021-10-11 14:54:11 -0300

    annotations

--- tests/test_shuffler.py
+++ tests/test_shuffler.py
@@ -6,13 +6,13 @@ class TestShuffler(unittest.TestCase):
     def setUp(self) -> None:
         self.image = Shuffler('images/test_image.png')
 
-    def test_original_image(self):
+    def test_original_image(self) -> None:
         self.assertRaises(ValueError, Shuffler, 'image')
 
-    def test_pixel_loss(self):
+    def test_pixel_loss(self) -> None:
         self.assertWarns(UserWarning, self.image.shuffle, (5, 5))
 
-    def test_matrix_values(self):
+    def test_matrix_values(self) -> None:
         values = [
             (2, 2, 2),
             (2, -2),
@@ -22,13 +22,13 @@ class TestShuffler(unittest.TestCase):
 
         for value in values:
             self.assertRaises(ValueError, self.image.shuffle, value)
-    
-    def test_matrix_final_shape(self):
+
+    def test_matrix_final_shape(self) -> None:
         self.image.shuffle((2, 2))
         shuffled_shape = self.image.shuffled.shape
         original_shape = self.image.original.shape
 
-        self.assertEqual(shuffled_shape, original_shape) 
+        self.assertEqual(shuffled_shape, original_shape)
 
 
 if __name__ == '__main__':
