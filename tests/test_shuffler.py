437ff793861dc3cf1ef516b4040464b431b9dcaf
Gabriel <storkdeveloper@gmail.com>
2021-10-11 11:41:20 -0300

    unit tests

---
+++ tests/test_shuffler.py
@@ -0,0 +1,35 @@
+from image_shuffler import Shuffler
+import unittest
+
+
+class TestShuffler(unittest.TestCase):
+    def setUp(self) -> None:
+        self.image = Shuffler('images/test_image.png')
+
+    def test_original_image(self):
+        self.assertRaises(ValueError, Shuffler, 'image')
+
+    def test_pixel_loss(self):
+        self.assertWarns(UserWarning, self.image.shuffle, (5, 5))
+
+    def test_matrix_values(self):
+        values = [
+            (2, 2, 2),
+            (2, -2),
+            (2, 2.1),
+            (2, 550),
+        ]
+
+        for value in values:
+            self.assertRaises(ValueError, self.image.shuffle, value)
+    
+    def test_matrix_final_shape(self):
+        self.image.shuffle((2, 2))
+        shuffled_shape = self.image.shuffled.shape
+        original_shape = self.image.original.shape
+
+        self.assertEqual(shuffled_shape, original_shape) 
+
+
+if __name__ == '__main__':
+    unittest.main()
